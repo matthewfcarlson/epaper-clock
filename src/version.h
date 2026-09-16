010479578764e09736cbd618604deccafc14c995
Matthew Carlson <matthewfcarlson@users.noreply.github.com>
2026-09-15 22:44:08 -0500

    v0.95

--- src/version.h
+++ src/version.h
@@ -2,4 +2,4 @@
 
 // Bump this and tag/publish a GitHub release with the same version (e.g. "v1.0.1")
 // whenever you want devices to pick up the build via the nightly OTA update check.
-#define FIRMWARE_VERSION "0.9.3"
+#define FIRMWARE_VERSION "0.9.5"
